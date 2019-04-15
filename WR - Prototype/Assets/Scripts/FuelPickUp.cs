3ae20fbd854be45ff8f499d9818bbd79b7e667fc
Max Hemberg <max.hemberg@gmail.com>
2019-04-16 00:35:06 +0200

    Fuel & Fuel UI Implemented

--- WR - Prototype/Assets/Scripts/FuelPickUp.cs
+++ WR - Prototype/Assets/Scripts/FuelPickUp.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class FuelPickUp : MonoBehaviour
 {
+    public float containerSize;
+    PlayerFuel playerFuelScript;
+
+    private void Start()
+    {
+        playerFuelScript = GameObject.Find("Player").GetComponent<PlayerFuel>();
+        CheckSizeFuelContainer();
+    }
+
+    private void CheckSizeFuelContainer()
+    {
+        //This is current size of fuel in camps, change if Fuel is made bigger. Use another "if" for the small size.
+        if (transform.localScale.x >= 0.2 && transform.localScale.y >= 0.2) {
+            containerSize = 25;
+        }
+        //if (transform.localScale.x >= 0.2 && transform.localScale.y >= 0.2) {
+        //  containerSize = 10;
+        //}
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            playerFuelScript.playerFuel += containerSize;
+           
             print("Player picked up the fuel!");
             Destroy(gameObject);
         }
---
+++ WR - Prototype/Assets/Scripts/PlayerFuel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerFuel : MonoBehaviour {
+
+    public float playerFuel;
+    public float fuelConsumption;
+    public Slider fuelUI;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        FuelManager();
+
+        //Set Fuel UI to value of playerFuel.
+        fuelUI.value = playerFuel;
+    }
+
+    private void FuelManager()
+    {
+        //Only consume fuel if you have fuel.
+        if (playerFuel > 0) {
+            playerFuel -= fuelConsumption * Time.deltaTime;
+        }
+
+        //Give console message, Out of Fuel and make sure fuel is exactly 0.
+        if (playerFuel <= 0) {
+            print("Out of Fuel");
+            playerFuel = 0;
+        }
+
+        //If Fuel is more than 100, make it 100.
+        if (playerFuel > 100) {
+            playerFuel = 100;
+        }
+    }
+}
