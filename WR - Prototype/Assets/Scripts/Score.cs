4d26daa17d0aec9d7fc050c68dbaa5923c38c50f
Unohu <Unohu@users.noreply.github.com>
2019-04-18 20:34:38 +0200

    Added score and CogPick ups have score value

---
+++ WR - Prototype/Assets/Scripts/Score.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+    int score;
+
+	void Update ()
+    {
+        score = Config.score;
+        scoreText.text = score.ToString();
+	}
+}
