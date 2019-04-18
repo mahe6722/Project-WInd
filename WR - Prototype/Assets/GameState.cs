3ce3fd9c4a3f5397d5537a7e178dbd183e2e3c78
Max Hemberg <max.hemberg@gmail.com>
2019-04-18 14:52:30 +0200

    Fuel Feedback 1.0 implemented. Restart Menu Foundation

---
+++ WR - Prototype/Assets/GameState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+    public bool gameOver;
+
+    public GameObject player;
+    public GameObject restartMenu;
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.activeInHierarchy == false) {
+            gameOver = true;
+        }
+
+        if (gameOver) {
+            restartMenu.SetActive(true);
+        }
+	}
+}
--- WR - Prototype/Assets/Scripts/PlayerFuel.cs
+++ WR - Prototype/Assets/Scripts/PlayerFuel.cs
@@ -8,11 +8,21 @@ public class PlayerFuel : MonoBehaviour {
     public float playerFuel;
     public float fuelConsumption;
     public Slider fuelUI;
+    public Image fuelBar;
+
+    float flashTimer;
+    public float flashDuration;
+    public float flashSpeed;
+    Color fuelBarStartColor;
+
 	
-    //void Start()
-    //{
+    void Start()
+    {
+        fuelBar = GameObject.Find("FuelBar").GetComponent<Image>();
+        fuelBarStartColor = fuelBar.color;
+
     //    playerFuel = Config.fuel;
-    //}
+    }
 
 	// Update is called once per frame
 	void Update ()
@@ -40,5 +50,33 @@ public class PlayerFuel : MonoBehaviour {
         if (playerFuel > 100) {
             playerFuel = 100;
         }
+
+        if (playerFuel < 40) {
+            flashTimer += Time.deltaTime;
+            if(playerFuel <= 20) {
+                flashSpeed = 8;
+            }
+            else if (playerFuel > 20) {
+                flashSpeed = 4;
+            }
+
+            if (fuelBar.color == fuelBarStartColor) {
+
+                fuelBar.color = new Color(0.45f, 0f, 0f, 1);
+
+            }
+
+            if (fuelBar.color != fuelBarStartColor && flashTimer >= flashDuration) {
+                fuelBar.color = Color.Lerp(fuelBar.color, fuelBarStartColor, (flashSpeed * 4) * Time.deltaTime);
+
+                if (fuelBar.color == fuelBarStartColor) {
+                    flashTimer = 0f;
+                }
+            }
+        }
+        else if (playerFuel > 40) {
+            fuelBar.color = fuelBarStartColor;
+        }
+        
     }
 }
