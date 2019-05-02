39aeb7f67730cd74d26d616649d21d2764587dc0
BjorneJosefsson <ludden1996@outlook.com>
2019-05-02 14:31:30 +0200

    123

--- WR - Prototype/Assets/Scripts/CogPickUp.cs
+++ WR - Prototype/Assets/Scripts/CogPickUp.cs
@@ -11,6 +11,9 @@ public class CogPickUp : MonoBehaviour
     public SpriteRenderer cogSprite;
     public CircleCollider2D cogCollider;
 
+    public GameObject particleEffect;
+    public GameObject pickupEffect;
+
     void Start()
     {
         configScript = GameObject.Find("DifficultySettings").GetComponent<Config>();
@@ -26,12 +29,16 @@ public class CogPickUp : MonoBehaviour
             cogSprite.enabled = false;
             cogCollider.enabled = false;
             cogSound.Play();
+            Instantiate(pickupEffect, transform.position, transform.rotation);
+
+ 
 
             print("Player picked up a cog!");
             configScript.score += cogValue;
 
             if (!cogSound.isPlaying) {
                 Destroy(gameObject);
+                Destroy(particleEffect);
             }
 
             
--- WR - Prototype/Assets/Scripts/PlayerCollisionDetection.cs
+++ WR - Prototype/Assets/Scripts/PlayerCollisionDetection.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerCollisionDetection : MonoBehaviour
 {
     GameObject player;
+    public GameObject ExplosionParticle;
+    public Screenshake cameraShake;
 
     void Start()
     {
@@ -21,11 +23,15 @@ public class PlayerCollisionDetection : MonoBehaviour
         {
             if (other.tag == "Fence" || other.tag == "Building")
             {
+                Instantiate(ExplosionParticle, transform.position, transform.rotation);
+                StartCoroutine(cameraShake.Shake(3f, 2f));
                 print("Player hit a fence or a bunker");
                 player.SetActive(false);
             }
             if (other.tag == "Obstacle")
             {
+                Instantiate(ExplosionParticle, transform.position, transform.rotation);
+                StartCoroutine(cameraShake.Shake(3f, 2f));
                 print("Player hit an obstacle");
                 player.SetActive(false);
             }
