0ef8b672971e49a1051a3c9752cbfb49036182a2
Max Hemberg <max.hemberg@gmail.com>
2019-05-08 16:12:39 +0200

    PlayerTrail Smoke Stutter

--- WR - Prototype/Assets/Scripts/PlayerTrailManager.cs
+++ WR - Prototype/Assets/Scripts/PlayerTrailManager.cs
@@ -9,6 +9,9 @@ public class PlayerTrailManager : MonoBehaviour {
 
     ParticleSystem.ColorOverLifetimeModule colorModule;
     ParticleSystem.MinMaxGradient startColor_Engine;
+
+    public float timer_EngineStutter;
+    public float duration_EngineStutter;
 	
 	void Start () {
 
@@ -19,9 +22,20 @@ public class PlayerTrailManager : MonoBehaviour {
 	}
 	
 	void Update () {
-		
+
+        timer_EngineStutter += Time.deltaTime;
+
         if (playerFuelScript.playerFuel < 30) {
             colorModule.color = Color.black;
+
+            if(timer_EngineStutter > duration_EngineStutter && engineTrail.isPlaying) {
+            engineTrail.Stop();
+             timer_EngineStutter = 0;
+            }
+
+            if (!engineTrail.isPlaying) {
+                engineTrail.Play();
+            }
         }
         else if(Time.timeScale < 1) {
             colorModule.color = Color.cyan;
@@ -29,6 +43,9 @@ public class PlayerTrailManager : MonoBehaviour {
 
         else if(playerFuelScript.playerFuel >= 30) {
             colorModule.color = startColor_Engine;
+            if (!engineTrail.isPlaying) {
+                engineTrail.Play();
+            }
         }
 
 	}
