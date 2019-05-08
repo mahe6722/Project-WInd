6e8aca6d443bf94825a4f8829a7b0d28d7ec4811
Max Hemberg <max.hemberg@gmail.com>
2019-05-08 14:52:02 +0200

    EngineParticleColorChanges

---
+++ WR - Prototype/Assets/Scripts/PlayerTrailManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrailManager : MonoBehaviour {
+
+    ParticleSystem engineTrail;
+    PlayerFuel playerFuelScript;
+
+    ParticleSystem.ColorOverLifetimeModule colorModule;
+    ParticleSystem.MinMaxGradient startColor_Engine;
+	
+	void Start () {
+
+       engineTrail = GetComponent<ParticleSystem>();
+       playerFuelScript = GameObject.Find("Player").GetComponent<PlayerFuel>();
+       colorModule = engineTrail.colorOverLifetime;
+       startColor_Engine = engineTrail.colorOverLifetime.color;
+	}
+	
+	void Update () {
+		
+        if (playerFuelScript.playerFuel < 30) {
+            colorModule.color = Color.black;
+        }
+        else if(Time.timeScale < 1) {
+            colorModule.color = Color.cyan;
+        }
+
+        else if(playerFuelScript.playerFuel >= 30) {
+            colorModule.color = startColor_Engine;
+        }
+
+	}
+}
