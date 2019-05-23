918ca4f7aef72eb74472e4c414da7ceb90386251
Unohu <Unohu@users.noreply.github.com>
2019-05-23 12:57:16 +0200

    NewScriptsInc

---
+++ WR - Prototype/Assets/Scripts/MousePosXLocator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePosXLocator : MonoBehaviour
+{
+    private Vector2 direction;
+	void Start ()
+    {
+        gameObject.transform.position = new Vector3(2, 5.5f, -10);
+	}
+	void Update ()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);      
+        //float mousePosX = mousePos.x;
+        direction = (mousePos - gameObject.transform.position).normalized;
+
+        gameObject.transform.position = new Vector2(Mathf.Clamp(transform.position.x, -5, 5), transform.position.y);
+
+        if (gameObject.transform.position.x >= -5 && gameObject.transform.position.x <= 5)
+        {
+            gameObject.transform.Translate(new Vector2(direction.x * Time.smoothDeltaTime * 100, 0));
+        }
+    }
+}
