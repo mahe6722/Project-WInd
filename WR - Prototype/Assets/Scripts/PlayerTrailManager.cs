dad78bae148334904a904fc13c8e68e4bfc69a6b
Unohu <Unohu@users.noreply.github.com>
2019-06-01 00:29:28 +0200

    Max's changes

--- WR - Prototype/Assets/Scripts/PlayerTrailManager.cs
+++ WR - Prototype/Assets/Scripts/PlayerTrailManager.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerTrailManager : MonoBehaviour {
+public class PlayerTrailManager : MonoBehaviour
+{
 
     public GameObject smokeEngineParticle;
     public GameObject engineParticle;
@@ -17,47 +18,58 @@ public class PlayerTrailManager : MonoBehaviour {
 
     public float timer_EngineStutter;
     public float duration_EngineStutter;
-	
-	void Start () {
 
-       engineTrail = GameObject.Find("newEngineTrail").GetComponent<ParticleSystem>();
-       smokeTrail = smokeEngineParticle.GetComponent<ParticleSystem>();
-       playerFuelScript = GameObject.Find("Player").GetComponent<PlayerFuel>();
-       colorModule = engineTrail.colorOverLifetime;
-       startColor_Engine = engineTrail.colorOverLifetime.color;
+    void Start()
+    {
+
+        engineTrail = GameObject.Find("newEngineTrail").GetComponent<ParticleSystem>();
+        smokeTrail = smokeEngineParticle.GetComponent<ParticleSystem>();
+        playerFuelScript = GameObject.Find("Player").GetComponent<PlayerFuel>();
+        colorModule = engineTrail.colorOverLifetime;
+        startColor_Engine = engineTrail.colorOverLifetime.color;
 
         playerPhaseShiftScript = GameObject.Find("Phase_Gadget").GetComponent<PlayerPhaseShift>();
-	}
-	
-	void Update () {
+    }
+
+    void Update()
+    {
 
         timer_EngineStutter += Time.deltaTime;
 
-        if (playerFuelScript.playerFuel < 0.3) {
+        if (playerFuelScript.playerFuel < 0.3)
+        {
             smokeEngineParticle.SetActive(true);
             engineParticle.SetActive(false);
 
-            if(timer_EngineStutter > duration_EngineStutter && smokeTrail.isPlaying) {
-            smokeTrail.Stop();
-             timer_EngineStutter = 0;
+            if (timer_EngineStutter > duration_EngineStutter && smokeTrail.isPlaying)
+            {
+                smokeTrail.Stop();
+                timer_EngineStutter = 0;
             }
 
-            if (!smokeTrail.isPlaying) {
+            if (!smokeTrail.isPlaying)
+            {
                 smokeTrail.Play();
             }
         }
-        else if(Time.timeScale < 1) {
+        else if (Time.timeScale < 1)
+        {
             colorModule.color = Color.cyan;
         }
 
-        else if(playerFuelScript.playerFuel >= 0.35 && playerPhaseShiftScript.portalExit > 0f) {
+        else if (playerFuelScript.playerFuel >= 0.35 && playerPhaseShiftScript.portalExit > 0f)
+        {
             colorModule.color = startColor_Engine;
             smokeEngineParticle.SetActive(false);
             engineParticle.SetActive(true);
-            if (!engineTrail.isPlaying) {
+            if (!engineTrail.isPlaying)
+            {
                 engineTrail.Play();
             }
         }
-
-	}
+        if (Time.timeScale >= 1)
+        {
+            colorModule.color = startColor_Engine;
+        }
+    }
 }
