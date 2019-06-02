02d1cb98805e4219c088eb810e0b89c6a48aeca4
Max Hemberg <max.hemberg@gmail.com>
2019-06-02 19:36:58 +0200

    Joystick Test Commit

---
+++ WR - Prototype/Assets/JoystickTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickTest : MonoBehaviour {
+
+    public float moveDirection;
+    public float joystickSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") > 0) {
+            print("joystick right");
+            moveDirection = Input.GetAxis("Horizontal");
+
+        }
+        if (Input.GetAxis("Horizontal") < 0) {
+            print("joystick left");
+            moveDirection = Input.GetAxis("Horizontal");
+        }
+
+        transform.Translate(moveDirection * joystickSpeed, 0, 0);
+    }
+}
