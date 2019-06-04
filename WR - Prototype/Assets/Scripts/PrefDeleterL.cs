6a25c2075050323290b309f39bd96a25ac363763
Unohu <Unohu@users.noreply.github.com>
2019-06-04 14:41:48 +0200

    SmallAlterations

---
+++ WR - Prototype/Assets/Scripts/PrefDeleterL.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PrefDeleterL : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L) && SceneManager.GetActiveScene().name == "GameScene")
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+}
---
+++ WR - Prototype/Assets/Scripts/RefreshGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class RefreshGame : MonoBehaviour
+{	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.K) && SceneManager.GetActiveScene().name == "GameScene")
+        {
+            SceneManager.LoadScene("NewMainMenu");
+        }
+	}
+}
